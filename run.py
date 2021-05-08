41602b2612872fef97eda9ad6b11a1f5f7db1768
mikohei <mikohei@amazon.co.jp>
2021-05-08 15:48:24 +0900

    [REMOVE] 重複起動の防止

--- run.py
+++ run.py
@@ -5,15 +5,6 @@ from time import sleep
 
 
 def main():
-    now = datetime.now()
-    dt_pkl = "datetime.pickle"
-    if os.path.exists(dt_pkl):
-        with open(dt_pkl, mode="rb") as f:
-            pre_dt = pickle.load(f)
-        if (now - pre_dt).seconds <= 60 * 60:
-            exit()
-    with open(dt_pkl, mode="wb") as dt:
-        pickle.dump(now - timedelta(minutes=5), dt)
     while True:
         os.system("python auto_reply.py")
         sleep(30)
